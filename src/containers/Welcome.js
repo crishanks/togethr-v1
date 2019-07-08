e1f8d64243361ffbe407fad4c133c02806fa90cc
github username <crishanks12@gmail.com>
2019-07-08 13:53:44 -0600

    added expo-font, async loading of fonts w/ logic in render, component is now class to handle lifecycle method, styled logo and get started link

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { StyleSheet, View, Text} from 'react-native';
 import { Link } from 'react-router-native';
+import * as Font from 'expo-font';
 
-export default function Welcome() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text>togethr</Text>
+export default class  Welcome extends React.Component {
+  state = {
+    fontLoaded: false
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      'raleway-extrabold': require('../../assets/fonts/Raleway-ExtraBold.ttf'),
+      'raleway-extralight': require('../../assets/fonts/Raleway-ExtraLight.ttf')
+    });
+    this.setState({fontLoaded: true})
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.main}>
+          {
+            this.state.fontLoaded ? (
+              <Text style={{fontFamily: 'raleway-extrabold', fontSize: 56, color: '#3185FC'}}>togethr</Text>
+            ) : null
+          }
+        </View>
+        <View style={styles.bottom}>
+          {
+            this.state.fontLoaded ? (
+              <Link to="/discover"><Text style={{color: 'white', fontFamily: 'raleway-extralight', fontSize: 24}}>Get Started</Text></Link>
+            ) : null
+          }
+        </View>
       </View>
-      <View style={styles.bottom}>
-        <Link to="/discover"><Text style={{color: 'white'}}>Get Started</Text></Link>
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -29,25 +52,8 @@ const styles = StyleSheet.create({
   bottom: {
     flex: 1,
     height: 5,
-    backgroundColor: 'black',
+    backgroundColor: '#403F4C',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   }
 });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: '#000'
-//   },
-//   bottom: {
-//     flex: 3,
-//     height: 100, 
-//     backgroundColor: 'black',
-//     justifyContent: 'center'
-//   },
-//   main: {
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     height: 300
-//   }
-// });
