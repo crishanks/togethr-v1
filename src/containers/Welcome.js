2493a338a4fd311c766be927c5a3788a60c49526
github username <crishanks12@gmail.com>
2019-07-08 11:35:10 -0600

    aligned view components, moved style to styles and adjusted layout

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -4,22 +4,50 @@ import { Link } from 'react-router-native';
 
 export default function Welcome() {
   return (
-      <View style={{flex: 3, height: 100, backgroundColor: 'black', justifyContent: 'center'}}>
-        <View style={{flex: 4, backgroundColor: 'white'}}>
-          <Text>Top Half</Text>
-        </View>
-        <View style={{alignItems: 'center', justifyContent: 'center', height: 300}}>
-          <Link to="/discover"><Text style={{color: 'white'}}>Get Started</Text></Link>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text>togethr</Text>
+      </View>
+      <View style={styles.bottom}>
+        <Link to="/discover"><Text style={{color: 'white'}}>Get Started</Text></Link>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#000'
+    flex: 1,
+    backgroundColor: 'white'
   },
-  link: {
-    color: 'white'
+  main: {
+    flex: 1,
+    height: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bottom: {
+    flex: 1,
+    height: 5,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: '#000'
+//   },
+//   bottom: {
+//     flex: 3,
+//     height: 100, 
+//     backgroundColor: 'black',
+//     justifyContent: 'center'
+//   },
+//   main: {
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     height: 300
+//   }
+// });
